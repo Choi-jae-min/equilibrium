704e32c3a5764f66ac97efa1562d60aa8242d9f0
jasmin <cjm98@smartel.co.kr>
2025-09-19 18:18:43 +0900

    feat

--- src/app/components/cards/productCard.tsx
+++ src/app/components/cards/productCard.tsx
@@ -12,12 +12,15 @@ const ProductCard:FC<Props> = ({title , type , img_src}) => {
     return (
         <div className={'group hover:cursor-pointer'}>
             <p className={'text-12 text-gray-500 pb-2'}>{type}</p>
-            <span className="flex items-center space-x-2 pb-3">
-                <h1 className="relative text-14 sm:text-18 font-semibold">{title}
-                    <span className="absolute left-0 -bottom-1 h-[1px] w-full bg-black scale-x-0 origin-left transition-transform duration-300 group-hover:scale-x-100"/>
-                </h1>
-                <MdOutlineArrowOutward className="transition-transform duration-300 ease-in-out group-hover:rotate-45" />
-           </span>
+            <span className="flex items-center space-x-2 pb-3 relative group overflow-hidden">
+              <h1 className="relative text-nowrap text-14 sm:text-18 font-semibold">
+                {title}
+                  <span className="absolute left-0 -bottom-1 h-[1px] w-full bg-black
+                                 scale-x-0 origin-left transition-transform duration-300
+                                 group-hover:scale-x-100 pointer-events-none"></span>
+              </h1>
+              <MdOutlineArrowOutward className="transition-transform duration-300 ease-in-out group-hover:rotate-45" />
+            </span>
 
 
             <div className={'relative w-full h-[400px] sm:h-[250px] lg:h-[500px] overflow-hidden group rounded-lg'}>
