ded84287093f20e87be2bf46088764a1575ca06f
jasmin <cjm98@smartel.co.kr>
2025-09-19 18:04:47 +0900

    update : text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,16 +25,17 @@ export default function Home() {
         <Header/>
         <main>
 
-          <p className={`italic`}>Redefining Balance in Coffee Culture</p>
+          <p className={`italic`}>어쩌구 저쩌구 주짓수 가야지</p>
           <p className={`italic`}>From EQBM .</p>
 
           <div className={'text-12 pt-10 flex justify-between items-end lg:hidden'}>
               <span>
-                  <p className="">3F, 7 Achasan-ro 30-gil, Gwangjin-gu, Seoul, Korea</p>
+                  <p className="">서울 대한민국 아차산로 30길-7 3층</p>
+                  {/*<p className="">3F, 7 Achasan-ro 30-gil, Gwangjin-gu, Seoul, Korea</p>*/}
                   <p className="font-mono">{time}</p>
                   <p className="">+0507 1332 6073</p>
               </span>
-              <p className={'font-mono flex items-end'}>from 2020.</p>
+              <p className={'font-mono flex items-end text-nowrap'}>from 2020.</p>
           </div>
 
 
@@ -60,7 +61,7 @@ export default function Home() {
                           className="object-cover transition-transform duration-500 ease-in-out group-hover:scale-105"
                       />
                       <ImageBtn
-                          title={'The Equilibrium Way'}
+                          title={'이퀼리 방향성'}
                           onClick={() => {}}
                           className="bg-white absolute top-5 right-5 px-4 py-2 shadow rounded-2xl hover:cursor-pointer"
                       />
